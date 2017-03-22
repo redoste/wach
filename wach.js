03e42830c9e4ac3f0a0a0fe1d77d5507a29816e3
JubCsc <jub.csc@gmail.com>
2017-03-22 14:21:22 +0100

    V0.3 Send

--- wach.js
+++ wach.js
@@ -1,6 +1,10 @@
 //Deffinition des variable globale
 var CHStatus = null;
 var CHBackup = null;
+var CHTESource = null;
+var CHTEPointAcc = null;
+var CHTEPointOld = null;
+var CHTEPointOldColor = null;
 
 //Fonction d'initialisation
 //0 Parametre
@@ -10,6 +14,11 @@ function CHInit(){
     //Definis le satatus de WACH
     //0 -> Classique
     //1 -> En atente de œ
+    //2 -> En atente de TextEdit
+    
+    CHTEPointOld = document.body
+    CHTEPointOldColor = document.body.style['background-color']
+    //Definis les variable globale de l'aide au pointage de text edit
     
     CHPWait()
     //Cree le bloc d'attete en bas de page
@@ -18,7 +27,11 @@ function CHInit(){
     //Sauvegarde les reponse
     
     document.onkeydown = CHRKey
+    document.onclick   = CHRClick
+    document.onmousemove = CHRMove
     //Definis l'écoute des touche du document vers CHRKey
+    //Definis l'écoute des clicks du document vers CHRClick
+    //Definis l'écoute de déplacement du document ver CHRMove
 }
 
 //Lis une réponse
@@ -99,6 +112,12 @@ function CHRKey(e){
         
         //Et on affiche le formulaire
         CHPEditForm()
+    }
+    //Le T en mode Wait
+    if (e.keyCode == 84 && CHStatus == 1){
+        //On passe en mode Text edit + suprime le message de wait
+        CHStatus = 2;
+        document.getElementById('CHPWait').style = 'display: none'
     }
 }
 
@@ -189,6 +208,133 @@ function CHPEditGet(){
 function CHPEditReset(id){
     //On récupère dans le CHBackup à l'id et on stoque dans la value de CHPEditForm_Q + id
     document.getElementById("CHPEditForm_Q" + id).value = CHBackup[id]
+}
+
+//Quand la souris Clique
+//1 parametre
+//e -> Objet d'event du navigateur
+function CHRClick(e){
+    //Si le mode de Text Edit est actié
+    if (CHStatus == 2){
+        //On Ouvrre le formulaire de TextEdit
+        CHPTeForm(e.target)
+        //Et on passe en mode classique
+        CHStatus = 0;
+    }
+}
+
+//Ouvre le formulaire de textedit
+//1 Parametre
+//source -> la ou l'utilisateur a clique
+//return -> le div de base du form
+function CHPTeForm(source){
+    //On sauvegarde la source
+    CHTESource = source;
+    
+    //On cree notre div de base id=CHPTeForm
+    var div = document.createElement('div')
+    div.id = "CHPTeForm"
+    
+    //Pour chaque enfant
+    for (var i=0; i<source.childNodes.length; i++){
+        //Si c'est du texte
+        if (source.childNodes[i].nodeName == "#text"){
+            //On ajoute une entrée pour lui du formulaire
+            //Un Input de type text
+            var txtIn = document.createElement('input')
+            txtIn.setAttribute('type', 'text')
+            
+            //Avec en id CHPTeForm_I + i
+            txtIn.setAttribute('id', 'CHPTeForm_I' + i)
+            
+            //Et en pré value le texte de base
+            txtIn.setAttribute('value', source.childNodes[i].textContent)
+            
+            //On merge
+            div.appendChild(txtIn)
+            
+            //+ saut de ligne
+            div.appendChild(document.createElement('br'))
+        }
+    }
+    
+    //On ajoute le boutton de save
+    var btn = document.createElement('button')
+    btn.innerText = "SAVE"
+    //Au click on Savegarde avec le contenu du form, on vide la source de backup,
+    //et on simule la fermeture du form avec HideFeedback de l'API de WA
+    btn.setAttribute('onclick', "CHPTeSave(CHPTeGet()); CHTESource = null; HideFeedback()")
+    
+    //Merge
+    div.appendChild(btn)
+    
+    //On affiche le tout avec ShowMessage de l'api de WA
+    ShowMessage(div.innerHTML)
+    
+    return div;
+    
+}
+
+//Recupere les entrée de CHPTeForm
+//0 Parametre
+//return -> Tableau -> [ID, TEXT, ID, TEXT]
+function CHPTeGet(){
+    //Variable de sortie
+    var ret = [];
+    
+    //Pour chaque enfant de la source
+    for (var i=0; i<CHTESource.childNodes.length; i++){
+        //On regarde si un champ pour lui existe
+        if (document.getElementById('CHPTeForm_I' + i) != undefined){
+            //On save tout
+            ret[ret.length] = i;
+            ret[ret.length] = document.getElementById('CHPTeForm_I' + i).value;
+        }
+    }
+    
+    return ret;
+}
+
+//Sauvegarde les entree de CHPTeForm
+//1 Parametre
+//entry -> Tableau -> [ID, TEXT, ID, TEXT]
+function CHPTeSave(entry){
+    //Pour chaque entrée Paire (les id)
+    for (var i=0; i < entry.length; i = i + 2){
+        //On stoque le tout dans la source
+        CHTESource.childNodes[entry[i]].textContent = entry[i+1]
+    }
+}
+
+//Lis le movement de la souris
+//1 Parametre
+//e -> objet event envoyer par le navigateur
+function CHRMove(e){
+    //Si le mode Text Edit est activé on met un repère de pointage
+    if(CHStatus == 2){
+        //Definis en tant que actuelle le pointé
+        CHTEPointAcc = e.target;
+        
+        //On restaure la couleur de l'ancien
+        CHTEPointOld.style["background-color"] = CHTEPointOldColor
+        
+        //Met l'actuelle en ancien et backup sa couleur
+        CHTEPointOld = CHTEPointAcc;
+        CHTEPointOldColor = CHTEPointAcc.style["background-color"]
+        
+        //On finit par le metre en gris
+        CHTEPointAcc.style["background-color"] = "grey"
+    }
+    //Si le mode Text Edit est déactivée et que Old n'est pas égale a sa variable offciele
+    else if(CHStatus != 2 && CHTEPointOld != document.body){
+        //On restaure la couleur de l'ancien
+        CHTEPointOld.style["background-color"] = CHTEPointOldColor
+        
+        //Et on reinit toute les variable
+        CHTEPointOld = document.body
+        CHTEPointOldColor = document.body.style['background-color']
+        CHTEPointAcc = null;
+    }
 }
 
 //INIT
